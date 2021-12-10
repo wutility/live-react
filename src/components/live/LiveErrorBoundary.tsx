78e5c5f0f2f19e3fdf0b206b2036d1f4e1f05f68
haikelfazzani <haikel.fazzani@hotmail.com>
2021-12-10 12:12:00 +0100

    2.0.0

--- src/components/live/LiveErrorBoundary.tsx
+++ src/components/live/LiveErrorBoundary.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export default function LiveErrorBoundary (Element, callback) {
+export default function LiveErrorBoundary (Element:any, callback:any) {
   return class ErrorB extends React.Component {
 
     state = { hasError: false, error: null };
 
-    static getDerivedStateFromError = error => {
+    static getDerivedStateFromError = (error:any) => {
       callback(error)
       return { hasError: true };
     };
 
-    componentDidCatch = (error) => {
+    componentDidCatch = (error:any) => {
       this.setState({ error, hasError: error });
     };
 
